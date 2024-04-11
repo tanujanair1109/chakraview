1511aea1d237f11d26fb392f9119646b6c461835
Tanuja Nair <tanujanair11@gmail.com>
2024-04-11 15:50:15 +0530

    Sample of Clerk authentication is done

---
+++ clerkk/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {ClerkProvider} from '@clerk/clerk-react'
+const{VITE_CLERK_PUBLISHABLE_KEY} = import.meta.env
+
+const publishablekey = VITE_CLERK_PUBLISHABLE_KEY
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  <React.StrictMode>
+    <ClerkProvider publishableKey={publishablekey}>
+    <App />
+    </ClerkProvider>
+  </React.StrictMode>,
+)
